9cbc7256f3b67414d56eb21b072d8a84742c9931
jhs512 <jangka512@gmail.com>
2025-09-12 08:26:29 +0900

    0086

--- back/src/main/kotlin/com/back/global/app/AppConfig.kt
+++ back/src/main/kotlin/com/back/global/app/AppConfig.kt
@@ -3,6 +3,7 @@ package com.back.global.app
 import com.back.domain.member.member.entity.BaseMember
 import com.back.domain.member.member.entity.Member
 import com.back.domain.member.member.repository.MemberAttrRepository
+import com.back.domain.member.member.repository.MemberRepository
 import com.back.domain.post.postUser.entity.PostUser
 import com.back.domain.post.postUser.repository.PostUserAttrRepository
 import com.back.standard.util.Ut
@@ -18,11 +19,13 @@ class AppConfig(
     environment: Environment,
     objectMapper: ObjectMapper,
     memberAttrRepository: MemberAttrRepository,
-    postUserAttrRepository: PostUserAttrRepository
+    postUserAttrRepository: PostUserAttrRepository,
+    memberRepository: MemberRepository,
 ) {
     init {
         Companion.environment = environment
         Ut.json.objectMapper = objectMapper
+        BaseMember.memberRepository = memberRepository
         BaseMember.memberAttrRepository = memberAttrRepository
         Member.attrRepository = memberAttrRepository
         PostUser.attrRepository = postUserAttrRepository
