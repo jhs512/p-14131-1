58b64dd68ed347b54e9ce878b248731b25257e2e
jhs512 <jangka512@gmail.com>
2026-01-22 14:21:25 +0900

    0029-2

--- front/src/global/backend/apiV1/schema.d.ts
+++ front/src/global/backend/apiV1/schema.d.ts
@@ -320,6 +320,40 @@ export interface components {
             /** Format: int64 */
             all: number;
         };
+        PageMemberWithUsernameDto: {
+            /** Format: int64 */
+            totalElements: number;
+            /** Format: int32 */
+            totalPages: number;
+            pageable: components["schemas"]["PageableObject"];
+            sort: components["schemas"]["SortObject"];
+            /** Format: int32 */
+            numberOfElements: number;
+            first: boolean;
+            last: boolean;
+            /** Format: int32 */
+            size: number;
+            content: components["schemas"]["MemberWithUsernameDto"][];
+            /** Format: int32 */
+            number: number;
+            empty: boolean;
+        };
+        PageableObject: {
+            paged: boolean;
+            /** Format: int32 */
+            pageNumber: number;
+            /** Format: int32 */
+            pageSize: number;
+            sort: components["schemas"]["SortObject"];
+            unpaged: boolean;
+            /** Format: int64 */
+            offset: number;
+        };
+        SortObject: {
+            sorted: boolean;
+            unsorted: boolean;
+            empty: boolean;
+        };
     };
     responses: never;
     parameters: never;
@@ -796,7 +830,7 @@ export interface operations {
                     [name: string]: unknown;
                 };
                 content: {
-                    "application/json;charset=UTF-8": components["schemas"]["MemberWithUsernameDto"][];
+                    "application/json;charset=UTF-8": components["schemas"]["PageMemberWithUsernameDto"];
                 };
             };
             /** @description Bad Request */
